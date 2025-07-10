87de1af30563a6439e529f323ad6b9aecd4a1027
BaoPham <macbook@PhamBaos-Macbook-pro.local>
2025-07-10 16:06:44 +0700

    init

---
+++ server/motor_rental/src/main/java/com/pqb/motor_rental/MotorRentalApplication.java
@@ -0,0 +1,13 @@
+package com.pqb.motor_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorRentalApplication.class, args);
+	}
+
+}
