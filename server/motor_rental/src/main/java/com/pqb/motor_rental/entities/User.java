b98e27477192bd999ae30e00768eb97ec3d5ef4f
BaoPham <macbook@PhamBaos-Macbook-pro.local>
2025-07-12 16:33:06 +0700

    properties, pom, build project, create connection with jpa, entities User

---
+++ server/motor_rental/src/main/java/com/pqb/motor_rental/entities/User.java
@@ -0,0 +1,106 @@
+package com.pqb.motor_rental.entities;
+
+import jakarta.persistence.*;
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    private String fullName;
+    private String email;
+    private String phone;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String avatarUrl;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    public User(String fullName, String email, String phone, String password, Role role) {}
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public AccountStatus getAccountStatus() {
+        return accountStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public void setAccountStatus(AccountStatus accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
